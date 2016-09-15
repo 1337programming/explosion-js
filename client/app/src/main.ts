ffffd84becf08c916c11dc1c9bfb712e8abf5457
Patrick Opie <opiepj@plu.edu>
2016-09-15 12:05:51 -0700

    Prod Mode

--- client/app/src/main.ts
+++ client/app/src/main.ts
@@ -1,14 +1,17 @@
-import {platformBrowser} from '@angular/platform-browser';
-
-
-declare let module: any;
-if (module.hot) {
-  module.hot.accept();
-}
-
-//platformBrowser().bootstrapModuleFactory(MainModule)
-
+import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app.module';
+
+declare let WEBPACK_PROD: string;
+declare let module: any;
+
+if (WEBPACK_PROD) {
+  enableProdMode();
+} else {
+  if (module.hot) {
+    module.hot.accept();
+  }
+}
+
 
 platformBrowserDynamic().bootstrapModule(AppModule);
