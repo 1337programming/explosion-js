7d49b9aa194d4d4bb4dda7733fad79d85aa179d2
Patrick Opie <opiepj@plu.edu>
2016-09-19 21:31:19 -0700

    hack

--- client/app/src/common/services/audio.service.ts
+++ client/app/src/common/services/audio.service.ts
@@ -7,33 +7,45 @@ export class Audio {
   
   constructor(@Inject('audioContext') private audioCtx) {
     this.compressor = this.audioCtx.createDynamicsCompressor();
-    this.compressor.threshold.value = -20;
-    this.compressor.knee.value = 10;
-    this.compressor.ratio.value = 12;
-    this.compressor.attack.value = 0;
-    this.compressor.release.value = 0.25;
-    // Try for lulz:
-    // this.compressor.release.value = 0;
-    this.compressor.connect(this.audioCtx.destination);
+    if (this.compressor) {
+      this.compressor.threshold.value = -20;
+      this.compressor.knee.value = 10;
+      this.compressor.ratio.value = 12;
+      this.compressor.attack.value = 0;
+      this.compressor.release.value = 0.25;
+      // Try for lulz:
+      // this.compressor.release.value = 0;
+      this.compressor.connect(this.audioCtx.destination);
+    }
   }
   
   public play(sample: AudioBuffer, panVal: number = 0) {
     const source: AudioBufferSourceNode = this.audioCtx.createBufferSource();
-    source.buffer = sample;
+    if (source) {
+      source.buffer = sample;
+    }
     
     const pan: StereoPannerNode = this.audioCtx.createStereoPanner();
-    pan.pan.value = panVal;
-    
-    source.connect(pan);
-    pan.connect(this.compressor);
-    
-    source.start(0);
-    
-    return () => {
-      source.stop(0);
-      source.disconnect();
-      pan.disconnect();
+    if (pan) {
+      pan.pan.value = panVal;
     }
+    
+    if (pan && source && this.compressor) {
+      source.connect(pan);
+      pan.connect(this.compressor);
+      source.start(0);
+  
+      return () => {
+        source.stop(0);
+        source.disconnect();
+        pan.disconnect();
+      }
+    } else {
+      return () => {
+        console.log('Audio is not supported');
+      }
+    }
+    
   }
   
   public pinkNoiseNode(): ScriptProcessorNode {
